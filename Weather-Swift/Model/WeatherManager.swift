6c8e162f9d68eadf85c89ef88dfb70f2bea3f073
Elberte Plínio <elbertedoolan@gmail.com>
2022-09-20 18:29:10 -0300

    Data now being decoded by class model

--- Weather-Swift/Model/WeatherManager.swift
+++ Weather-Swift/Model/WeatherManager.swift
@@ -13,31 +13,38 @@ struct WeatherManager {
     //Completes the URL that user enters in the search text field
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString)
+        performRequest(with: urlString)
     }
     
     //Check for results in the OpenWeather API
-    func performRequest(_ urlString: String) {
+    func performRequest(with urlString: String) {
         if let url = URL(string: urlString) {
             
             let session = URLSession(configuration: .default)
             
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            let task = session.dataTask(with: url) { data, response, error in
+                //This code handle the data we receive back from OpenWeather API
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(with: safeData)
+                }
+            }
             
             task.resume()
         }
     }
     
-    //This function handle the data we receive back from OpenWeather API
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(with weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
 }
